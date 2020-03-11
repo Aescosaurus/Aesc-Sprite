18342cc1e2877dbff4cf4c363e29263406fa01c2
Aescosaurus <michaelbuslik@hotmail.com>
2020-03-11 12:30:17 -0600

    Saved resource.

--- AescSprite/resource.h
+++ AescSprite/resource.h
@@ -13,6 +13,9 @@
 #define IDC_AESCSPRITE                  109
 #define IDR_MAINFRAME                   128
 #define ID_FILE_OPEN                    32771
+#define ID_FILE_SAVE                    32778
+#define ID_FILE_SAVE32781               32781
+#define ID_FILE_SAVEAS                  32782
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -21,7 +24,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_COMMAND_VALUE         32786
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
